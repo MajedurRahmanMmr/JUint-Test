ff468711f2c7844a365a31af497c5fccf21e8815
MajedurRahman <Majedur.rahman.mmr@gmail.com>
2019-02-09 19:34:22 +0600

    End Of Problem 2 Solution and Start of Problem 2

--- src/Operation.java
+++ src/Operation.java
@@ -1,6 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Operation {
     public int sumTwoInteger(int i, int j) {
         int sum = (i + j);
         return sum < 0 ? 0 : sum;
+    }
+
+
+    public List<Integer> sortAndMakeUniqueItemList(List<Integer> asList) {
+
+        if (asList.isEmpty()) {
+            return asList;
+        }
+
+        if (asList.size() > 10) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        Collections.sort(asList);
+
+        asList = asList.stream().distinct().collect(Collectors.toList());
+
+        return asList;
     }
 }
--- src/OperationTest.java
+++ src/OperationTest.java
@@ -1,19 +1,37 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class OperationTest {
 
+    Operation operation = new Operation();
 
     @Test
     public void sumTwoInteger() {
-        Operation oparation = new Operation();
 
-        assertEquals(11, oparation.sumTwoInteger(5, 6));
-        assertEquals(1, oparation.sumTwoInteger(-5, 6));
+        assertEquals(11, operation.sumTwoInteger(5, 6));
+        assertEquals(1, operation.sumTwoInteger(-5, 6));
 
-        assertEquals(0, oparation.sumTwoInteger(5, -6));
-        assertEquals(0, oparation.sumTwoInteger(-15, -61));
+        assertEquals(0, operation.sumTwoInteger(5, -6));
+        assertEquals(0, operation.sumTwoInteger(-15, -61));
+
+    }
+
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void sortAndMakeUniqueItemListTest() {
+        List<Integer> integerList = asList(1, -3, 4, -5, 6, -5);
+        operation.sortAndMakeUniqueItemList(integerList);
+        List<Integer> emtyList = new ArrayList<>();
+        assertEquals(emtyList, operation.sortAndMakeUniqueItemList(emtyList));
+        List<Integer> largeList = asList(1, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6, 3);
+        assertEquals(asList(1, 3, 5, 6), operation.sortAndMakeUniqueItemList(largeList));
+        assertEquals(asList(-5, -3, 1, 4, 6), operation.sortAndMakeUniqueItemList(integerList));
 
     }
 }
